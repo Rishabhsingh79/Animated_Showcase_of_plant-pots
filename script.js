1e7ee9069295360f2383b7146cb3e707a9ab2e52
Rishabhsingh79 <98397014+Rishabhsingh79@users.noreply.github.com>
2024-07-25 21:55:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,155 @@
+gsap.from("#image",{
+    x: '1000px',
+    duration: 3,
+    stagger: .2,
+    ease: 'power2.in'
+})
+
+gsap.from("#toplft h1",{
+    x: '-100px',
+    duration: 4,
+    ease: Expo.easeinOut
+})
+
+gsap.from("#arrow",{
+    x: '100px',
+    duration: 3,
+    delay: 3,
+    ease: Expo.easeOut
+})
+
+gsap.from("#icon1",{
+    y: '-100px',
+    duration: 3,
+    delay: 5,
+    ease: Bounce.easeOut
+})
+
+gsap.from("#data1",{
+    x: '200px',
+    duration: 1,
+    opacity: 0,
+    delay: 6,
+    ease: Expo.easeOut
+})
+gsap.from("#data2",{
+    x: '200px',
+    duration: 1,
+    opacity: 0,
+    delay: 7,
+    ease: Expo.easeOut
+})
+gsap.from("#data3",{
+    x: '200px',
+    duration: 1,
+    opacity: 0,
+    delay: 8,
+    ease: Expo.easeOut
+})
+gsap.from("#data4",{
+    x: '200px',
+    duration: 2,
+    opacity: 0,
+    delay: 9,
+    ease: Expo.easeOut
+})
+gsap.from("#data5",{
+    x: '200px',
+    duration: 3,
+    opacity: 0,
+    delay: 10,    
+    ease: Expo.easeOut
+})
+gsap.from("#data6",{
+    x: '200px',
+    duration: 3,
+    opacity: 0,
+    stagger: 2,
+    delay: 11,
+    ease: Expo.easeOut
+})
+
+gsap.from("#arrow2",{
+    x: '-90px',
+    duration: 3,
+    delay: 5,
+    ease: Expo.easeOut
+})
+
+gsap.from("#btmlft h6",{
+    y: '-100px',
+    duration: 3,
+    delay: 12,
+    ease: Bounce.easeOut
+})
+
+gsap.from("#rubber h1",{
+    x: '-100px',
+    duration: 3,
+    opacity: 0,
+    stagger: 2,
+    delay: 13,
+    ease: Expo.easeinOut
+})
+
+gsap.from("#rubber p",{
+    x: '100px',
+    duration: 3,
+    opacity: 0,
+    stagger: 2,
+    delay: 14,
+    ease: Expo.easeinOut
+})
+
+gsap.from("#btmlft h3",{
+    x: '-100px',
+    duration: 3,
+    opacity: 0,
+    stagger: 2,
+    delay: 15,
+    ease: Expo.easeInOut
+})
+
+gsap.from("#light1",{
+    x: '100px',
+    duration: 3,
+    opacity: 0,
+    delay: 17,
+    ease: Bounce.easeOut
+})
+gsap.from("#light2",{
+    x: '100px',
+    duration: 3,
+    opacity: 0,
+    delay: 20,
+    ease: Bounce.easeOut
+})
+gsap.from("#light3",{
+    x: '100px',
+    duration: 3,
+    opacity: 0,
+    delay: 19,
+    ease: Bounce.easeOut
+})
+gsap.from("#light4",{
+    x: '100px',
+    duration: 3,
+    opacity: 0,
+    delay: 18,
+    ease: Bounce.easeOut
+})
+
+gsap.from("#btmrgt",{
+    x: '-50px',
+    duration: 3,
+    opacity: 0,
+    delay: 6,
+    ease: Expo.easeOut
+})
+
+gsap.from("#about",{
+    y: '-500px',
+    duration: 3,
+    delay: 6,
+    ease: Expo.easeOut
+})
